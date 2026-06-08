40b87ab6982266b6ea3327b922090ffa6b68e43c
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-06-08 11:52:36 +0200

    feat : complete exercice 2

---
+++ jour01/job02/my_putchar.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int my_putchar(char c) { 
+
+    write(1, &c, sizeof(c)); 
+
+    return 0;
+}
+
+void my_putstr(char *c) { 
+    while (*c != 0) {
+        my_putchar(*c);
+        c++; 
+    }
+}
+
+int main() { 
+
+    my_putchar('a');
+    my_putchar(' ');
+    my_putchar('8');
+    my_putchar('!');
+
+    char str[] = "\nHello, World!\n";
+    my_putstr(str);
+
+    return 0;
+}
