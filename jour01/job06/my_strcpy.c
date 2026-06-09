b961fc4406409797f4b32035d05de2dfdad75249
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-06-09 10:09:20 +0200

    feat : complete exercice 6

--- jour01/job06/my_strcpy.c
+++ jour01/job06/my_strcpy.c
@@ -4,3 +4,28 @@ qui copie la source dans la destination.
 char *my_strcpy(char *dst, char *src);
 
 Fonctions autorisées : aucune. */
+
+#include <stdio.h>
+
+char *my_strcpy(char *dst, char *src)
+{
+    int i = 0; // index to traverse both strings
+
+    while (src[i]) // loop until null terminator
+    {
+        dst[i] = src[i]; // copy current character
+        i++;
+    }
+    dst[i] = '\0'; // null-terminate the destination
+
+    printf("%s", dst);
+    return (dst);
+}
+
+int main()
+{
+    char copied_string[] = "Hello C!";  // source string
+    char destination_string[9];         // destination buffer, sized for source + '\0'
+
+    my_strcpy(destination_string, copied_string); // copy source into destination
+}
