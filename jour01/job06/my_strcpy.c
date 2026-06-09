0bb426d34eb1c75a360374b76d7b80d8630dd129
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-06-09 09:32:32 +0200

    docs : add exercice instructions to job 06 07

---
+++ jour01/job06/my_strcpy.c
@@ -0,0 +1,6 @@
+/* Dans un fichier my_strcpy.c, implémenter la fonction my_strcpy qui prend
+en paramètres une chaîne de caractères de destination et une de source, et
+qui copie la source dans la destination.
+char *my_strcpy(char *dst, char *src);
+
+Fonctions autorisées : aucune. */
---
+++ jour01/job07/my_strcmp.c
@@ -0,0 +1,13 @@
+/* Dans un fichier my_strcmp.c, implémenter la fonction my_strcmp qui prend
+qui prend en paramètre deux chaînes de caractères, les compare, et renvoie
+une valeur négative si la première chaîne est inférieure à la deuxième,
+positive si elle est supérieure à la deuxième, et 0 si elles sont identiques :
+int my_strcmp(char *s1, char *s2);
+Fonctions autorisées : aucune.
+
+Note :
+Une chaîne est “inférieure” à une autre si le premier caractère différent dans
+les deux chaînes est inférieur (dans l’ordre de la table ASCII) dans la première
+chaîne par rapport à celui de la deuxième chaîne. Par exemple, la chaîne
+“abc” est inférieure à la chaîne “abd” parce que les deux premiers caractères
+sont identiques, mais “c” est inférieur à “d” dans la table ASCII. */
