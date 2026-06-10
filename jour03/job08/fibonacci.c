012591e95ec019c039832fcde814a49f54a8440d
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-06-10 17:32:35 +0200

    fixfeat : debug iterative fibonacci, make recursive fibonacci

--- jour03/job08/fibonacci.c
+++ jour03/job08/fibonacci.c
@@ -9,35 +9,33 @@ Fonctions autorisées : aucune. */
 
 #include "fibonacci.h"
 
-int fibonacci_it (int n) 
-{   
-    int temp;
-    int f_n;
-    int f_n_minus_1 = 1; 
-    int f_n_minus_2 = 0;
-    
-    if (n == 0); 
-    {
-        return 0;
-    }
+int fibonacci_it(int n)
+{
+    int f_prev = 0;
+    int f_curr = 1;
+    int f_next;
 
-    if (n == 1); 
-    {
+    if (n == 0)
+        return 0;
+    if (n == 1)
         return 1;
-    }
 
     for (int i = 2; i <= n; i++)
-    {   
-        temp = f_n;
-        f_n = f_n_minus_1 + f_n_minus_2;
-        f_n_minus_2 = f_n_minus_1;
-        f_n_minus_1 = temp;
+    {
+        f_next = f_curr + f_prev;
+        f_prev = f_curr;
+        f_curr = f_next;
     }
 
+    return f_curr;
 }
 
-int fibonacci_rec (int n)
+int fibonacci_rec(int n)
 {
-    int f0 = 0;
-    int f1 = 1;
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2);
 }
--- jour03/job08/main.c
+++ jour03/job08/main.c
@@ -1,3 +1,15 @@
 #include "fibonacci.h"
 
 #include <stdio.h>
+
+int main() 
+{   
+    printf("%d\n",fibonacci_it(0)); 
+    printf("%d\n",fibonacci_it(1)); 
+    printf("%d\n",fibonacci_it(6)); 
+
+    printf("%d\n",fibonacci_rec(0)); 
+    printf("%d\n",fibonacci_rec(1)); 
+    printf("%d\n",fibonacci_rec(6)); 
+    return 0;
+}
